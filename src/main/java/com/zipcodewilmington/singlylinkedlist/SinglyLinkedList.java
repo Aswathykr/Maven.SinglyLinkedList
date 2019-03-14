766c848acf032f00362c9b39bbf712eada8bad6b
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-03-13 21:53:17 -0400

    implemented and tested all but sort

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,131 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T extends Comparable<T> > {
+
+    Node<T> headNode;
+
+    public void add(T item){
+        Node<T> node= new Node<T>(item);
+        if(headNode == null){
+            headNode = node;
+        }
+        else{
+            Node<T> last = getLastNode();
+            last.next = node;
+        }
+    }
+
+    private Node<T> getLastNode() {
+        Node<T> lastNode = null;
+        if(headNode != null) {
+            lastNode = headNode;
+            while (lastNode.next != null) {
+                lastNode = lastNode.next;
+            }
+        }
+        return lastNode;
+    }
+
+    private Node<T> getPrevNode(int index) {
+        Node<T> node = null;
+        if(headNode != null) {
+            node = headNode;
+            int count = 0;
+            while (node != null && count != index - 1) {
+                node = node.next;
+                count++;
+            }
+        }
+        return node;
+    }
+
+    public void remove(int index){
+        if(index == 0){
+            headNode = headNode.next;
+        }
+        else{
+            Node<T> node = getPrevNode(index);
+            if(node != null) {
+                Node<T> temp = node.next;
+                node.next = temp.next;
+            }
+        }
+    }
+
+    public boolean contains(T item){
+        return find(item) != -1;
+    }
+
+    public int find(T item){
+        Node<T> node = null;
+        int index = -1;
+        if(headNode != null) {
+            node = headNode;
+            while (node != null) {
+                index++;
+                if(node.data.equals(item)){
+                    break;
+                }
+                node = node.next;
+            }
+            if(node == null){
+                index = -1;
+            }
+        }
+        return index;
+    }
+
+    public int size(){
+        Node<T> lastNode = null;
+        int index = 0;
+        if(headNode != null) {
+            lastNode = headNode;
+            while (lastNode != null) {
+                index++;
+                lastNode = lastNode.next;
+            }
+        }
+        return index;
+    }
+
+    public T get(int index){
+        T data = null;
+        if(index == 0 && headNode != null){
+            data = headNode.data;
+        }else{
+            Node<T> prevNode = getPrevNode(index);
+            if(prevNode != null && prevNode.next != null)
+                data = prevNode.next.data;
+        }
+        return data;
+    }
+
+    public SinglyLinkedList<T> copy(){
+        SinglyLinkedList listCopy = new SinglyLinkedList();
+        Node<T> node = null;
+        int index = 0;
+        if(headNode != null) {
+            node = headNode;
+            while (node != null) {
+                index++;
+                listCopy.add(node.data);
+                node = node.next;
+            }
+        }
+        return listCopy;
+    }
+
+    public void sort(){
+
+    }
+
+    private class Node<T>{
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,244 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void addtoHeadTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valueToadd = new Integer(5);
+        intList.add(valueToadd);
+
+        Integer retrievedValue = intList.get(0);
+        Assert.assertEquals(retrievedValue, valueToadd);
+    }
+
+    @Test
+    public void addtoLastTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+
+        Integer[] retrievedValue = new Integer[valuesToadd.length];
+        for (int i = 0; i < valuesToadd.length; i++) {
+            retrievedValue[i] = intList.get(i);
+            Assert.assertEquals(retrievedValue[i], valuesToadd[i]);
+        }
+    }
+
+    @Test
+    public void removeFromHeadTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        intList.remove(0);
+        Integer retrievedValue = intList.get(0);
+        Assert.assertEquals(retrievedValue, valuesToadd[1]);
+    }
+
+    @Test
+    public void removeFromMiddleTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        intList.remove(1);
+        Integer retrievedValue = intList.get(1);
+        Assert.assertEquals(retrievedValue, valuesToadd[2]);
+    }
+
+    @Test
+    public void removeFromEndTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        intList.remove(2);
+        Integer retrievedValue = intList.get(2);
+        Assert.assertNull(retrievedValue);
+    }
+
+    @Test
+    public void containsInHeadTrueTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        boolean retrievedResult = intList.contains(5);
+        Assert.assertTrue(retrievedResult);
+
+    }
+
+    @Test
+    public void containsInMiddleTrueTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        boolean retrievedResult = intList.contains(3);
+        Assert.assertTrue(retrievedResult);
+
+    }
+
+    @Test
+    public void containsInEndTrueTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        boolean retrievedResult = intList.contains(6);
+        Assert.assertTrue(retrievedResult);
+
+    }
+
+    @Test
+    public void containsFalseTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        boolean retrievedResult = intList.contains(8);
+        Assert.assertFalse(retrievedResult);
+
+    }
+
+    @Test
+    public void findInHeadTrueTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        int retrievedResult = intList.find(5);
+        Assert.assertEquals(retrievedResult, 0);
+
+    }
+
+    @Test
+    public void findInMiddleTrueTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        int retrievedResult = intList.find(3);
+        Assert.assertEquals(retrievedResult, 1);
+
+    }
+
+    @Test
+    public void findInEndTrueTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        int retrievedResult = intList.find(6);
+        Assert.assertEquals(retrievedResult, 2);
+
+    }
+
+    @Test
+    public void findFalseTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        int retrievedResult = intList.find(8);
+        Assert.assertEquals(retrievedResult, -1);
+
+    }
+
+    @Test
+    public void sizeTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        int retrievedResult = intList.size();
+        Assert.assertEquals(retrievedResult, 3);
+
+    }
+    @Test
+    public void sizeFalseTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+
+        int retrievedResult = intList.size();
+        Assert.assertEquals(retrievedResult, 0);
+
+    }
+
+    @Test
+    public void getInHeadTrueTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        Integer retrievedResult = intList.get(0);
+        Assert.assertEquals(retrievedResult, valuesToadd[0]);
+
+    }
+
+    @Test
+    public void getInMiddleTrueTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        Integer retrievedResult = intList.get(1);
+        Assert.assertEquals(retrievedResult, valuesToadd[1]);
+
+    }
+
+    @Test
+    public void getInEndTrueTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        Integer retrievedResult = intList.get(2);
+        Assert.assertEquals(retrievedResult, valuesToadd[2]);
+
+    }
+
+    @Test
+    public void getFalseTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+        Integer retrievedResult = intList.get(3);
+        Assert.assertNull(retrievedResult);
+
+    }
+
+    @Test
+    public void copyTest(){
+        SinglyLinkedList<Integer> intList = new SinglyLinkedList<Integer>();
+        Integer valuesToadd[] ={ 5,3,6};
+        Arrays.stream(valuesToadd)
+                .forEach(value -> intList.add(value));
+
+        SinglyLinkedList<Integer> copyList= intList.copy();
+        Assert.assertEquals(copyList.get(0), valuesToadd[0]);
+        Assert.assertEquals(copyList.get(1), valuesToadd[1]);
+        Assert.assertEquals(copyList.get(2), valuesToadd[2]);
+
+        intList.remove(0); // This should modify only the intList
+
+        Assert.assertEquals(intList.get(0), valuesToadd[1]);
+        Assert.assertEquals(copyList.get(0), valuesToadd[0]);
+
+    }
+
+    public void sortTest(){
+
+    }
+
 }
